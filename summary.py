e54dd611ceb307db1358d12bae96c6e272b788f3
Krishan098 <168003081+Krishan098@users.noreply.github.com>
2025-04-10 22:31:41 +0530

    Delete summary.py

--- summary.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-df=pd.read_csv('dataset.csv')
-print(df.describe())
-print("columns:",list(df.columns))
-print("Total comments: ",len(df))
-print("Missing values:",df.isnull().sum())
